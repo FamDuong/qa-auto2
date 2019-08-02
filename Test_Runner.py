c1db95fa77825d72a3d4481c9ad07c2a199a7f44
cuongld <cuongld@coccoc.com>
2019-08-02 14:08:42 +0700

    change test rail config

--- Test_Runner.py
+++ Test_Runner.py
@@ -1,7 +1,8 @@
+import pytz
+from pytest_testrail.plugin import pytestrail
+
 from models.pageobject.downloads import DownloadsPageObject
 from models.pageobject.version import VersionPageObject
-from utils.const import Urls
-from selenium import webdriver
 
 
 class TestBrowser:
@@ -12,15 +13,9 @@ class TestBrowser:
         element = browser.execute_script('return arguments[0].shadowRoot', selector)
         return element
 
-    def test_current_time_now(self, browser):
-        browser.get(Urls.COCCOC_DOWNLOAD_URL)
-        self.download_page_object.cancel_all_current_torrent(browser)
-        self.download_page_object.clear_all_existed_torrent(browser)
+    @pytestrail.case('C36161')
+    def test_current_time_now(self):
+        assert True
 
-    def test_get_profile_path(self):
-        driver = webdriver.Chrome()
-        driver.maximize_window()
-        driver.get(Urls.COCCOC_VERSION_URL)
-        full_path_value = self.version_page_object.get_profile_path(driver)
-        print(full_path_value)
-
+    def test_timezone(self):
+        print(pytz.all_timezones)
--- conftest.py
+++ conftest.py
@@ -8,32 +8,32 @@ from simple_settings import settings
 driver = None
 
 
-# @pytest.mark.hookwrapper
-# def pytest_runtest_makereport(item):
-#     """
-#         Extends the PyTest Plugin to take and embed screenshot in html report right before close webdriver.
-#         :param item:
-#         """
-#     pytest_html = item.config.pluginmanager.getplugin('html')
-#     outcome = yield
-#     report = outcome.get_result()
-#     extra = getattr(report, 'extra', [])
-#
-#     if report.when == 'call':
-#         hasattr(report, 'wasxfail')
-#         # file_name = report.nodeid.replace("::", "_") + ".png"
-#         timestamp = datetime.now().strftime('%H-%M-%S.%f')[:-3]
-#         filename = timestamp + ".png"
-#         _capture_screenshot(filename)
-#         # if file_name:
-#         html = '<div><img src="screenshots/%s" style="width:600px;height:228px;" ' \
-#                    'onclick="window.open(this.src)" align="right"/></div>' % filename
-#         extra.append(pytest_html.extras.html(html))
-#     report.extra = extra
-#
-#
-# def _capture_screenshot(filename):
-#     driver.save_screenshot("screenshots/" + filename)
+@pytest.mark.hookwrapper
+def pytest_runtest_makereport(item):
+    """
+        Extends the PyTest Plugin to take and embed screenshot in html report right before close webdriver.
+        :param item:
+        """
+    pytest_html = item.config.pluginmanager.getplugin('html')
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, 'extra', [])
+
+    if report.when == 'call':
+        hasattr(report, 'wasxfail')
+        # file_name = report.nodeid.replace("::", "_") + ".png"
+        timestamp = datetime.now().strftime('%H-%M-%S.%f')[:-3]
+        filename = timestamp + ".png"
+        _capture_screenshot(filename)
+        # if file_name:
+        html = '<div><img src="screenshots/%s" style="width:600px;height:228px;" ' \
+                   'onclick="window.open(this.src)" align="right"/></div>' % filename
+        extra.append(pytest_html.extras.html(html))
+    report.extra = extra
+
+
+def _capture_screenshot(filename):
+    driver.save_screenshot("screenshots/" + filename)
 
 
 @pytest.fixture(scope='session')
@@ -52,15 +52,7 @@ def browser():
         chrome_options.add_argument('--ignore-certificate-errors')
         chrome_options.add_argument("--allow-insecure-localhost")
         chrome_options.add_argument("--disable-infobars")
-        # chrome_options.add_argument("--disable-popup-blocking")
-        # chrome_options.add_experimental_option("browser.download.panel.shown", False)
         chrome_options.add_argument('--user-data-dir=' + settings.USER_DATA_DIR)
-        # chrome_options.add_experimental_option("browser.helperApps.neverAsk.openFile","text/csv,application/vnd.ms-excel")
-        # chrome_options.add_experimental_option("browser.helperApps.neverAsk.saveToDisk", "text/csv,application/vnd.ms-excel")
-
-        # capabilities = DesiredCapabilities.CHROME
-        # capabilities['acceptSslCerts'] = True
-        # capabilities['acceptInsecureCerts'] = True
 
         driver = webdriver.Chrome(options=chrome_options)
         driver.maximize_window()
