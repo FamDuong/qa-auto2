af62af2c2d5129f317872d378290845a000fd1f1
cuongld <cuongld@coccoc.com>
2019-08-08 13:50:18 +0700

    Add requirements for libs

--- utils/winappdriver/test_win_app.py
+++ utils/winappdriver/test_win_app.py
@@ -13,24 +13,25 @@ class TestSimpleCalculator:
     #     displaytext = displaytext.lstrip(' ')
     #     return displaytext
 
+    def check_savior_icon(self, driver):
+        try:
+            is_enabled = driver.find_element_by_name('Downloadable media found!')
+        # actions = ActionChains(win_app_driver)
+        # actions.move_to_element(is_enabled)
+        # actions.double_click()
+        # actions.perform()
+            is_enabled.click()
+        except:
+            return 'not found'
+
     def test_initialize(self, win_app_driver):
-        time.sleep(1)
-        url_bar = win_app_driver.find_element_by_name('Address and search bar')
-        url_bar.click()
-        url_bar.send_keys('https://www.youtube.com/watch?v=WZDMQOOH_ZE&list=RDWZDMQOOH_ZE&start_radio=1')
-        url_bar.send_keys(Keys.RETURN)
         time.sleep(10)
-        is_enabled = win_app_driver.find_element_by_class_name('Chrome_WidgetWin_1')
-        actions = ActionChains(win_app_driver)
-        actions.move_to_element(is_enabled)
-        actions.double_click()
-        actions.perform()
-        time.sleep(3)
-        is_enabled.is_selected()
-        print('Verify this item:', is_enabled.is_selected())
-        time.sleep(2)
-        # win_app_driver.find_element_by_name("Clear").click()
-        # win_app_driver.find_element_by_name("Seven").click()
-        # assert self.getresults(win_app_driver) == "7"
-        # win_app_driver.find_element_by_name("Clear").click()
+        # url_bar = win_app_driver.find_element_by_name('Address and search bar')
+        # url_bar.click()
+        # url_bar.send_keys('https://git.itim.vn/users/sign_in')
+        # url_bar.send_keys(Keys.RETURN)
+        value = self.check_savior_icon(win_app_driver)
+        assert value == 'not found'
+
+
 
