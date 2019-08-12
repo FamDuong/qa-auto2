d7eba499a8e5cf7c346d014c5d7b7c45dae3d533
cuongld <cuongld@coccoc.com>
2019-08-12 09:17:46 +0700

    Add test for install and uninstall coccoc browser

--- models/pagelocators/version.py
+++ models/pagelocators/version.py
@@ -4,3 +4,4 @@ from selenium.webdriver.common.by import By
 class VersionPageLocators(object):
 
     PROFILE_PATH_ELEMENT = (By.ID, 'profile_path')
+    FLASH_PATH_ELEMENT = (By.ID, 'flash_version')
