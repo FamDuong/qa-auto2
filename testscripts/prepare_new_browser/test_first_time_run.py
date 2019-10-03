7e2b401fce127c26e305f139c63e7d9bcd763c04
bichngoc <bichngoc@coccoc.com>
2019-10-03 09:27:06 +0700

    QA-441: trial on jenkins

--- testscripts/prepare_new_browser/test_first_time_run.py
+++ testscripts/prepare_new_browser/test_first_time_run.py
@@ -4,12 +4,13 @@ from pytest_testrail.plugin import pytestrail
 from utils_automation.const import Urls
 from models.pageobject.settings import SettingsPageObject
 from models.pageelements.settings import SettingsPageLocators
-from utils_automation.common import BrowserHandler, WindowsCMD, wait_for_stable, FilesHandle
+from utils_automation.common import BrowserHandler, WindowsCMD, wait_for_stable, FilesHandle, WindowsHandler
 
 class TestFirstRun(TestInstall):
 
     new_browser = BrowserHandler()
     setting_page_object = SettingsPageObject()
+    windows = WindowsHandler()
     file = FilesHandle()
 
     @pytestrail.case('C44830')
@@ -66,3 +67,16 @@ class TestFirstRun(TestInstall):
             print(signatures[i])
             assert "COC COC COMPANY LIMITED" in str(signatures[i])
 
+    @pytestrail.case('C44840')
+    def test__rule_in_firewall_of_windows_if_user_selects_allow_access_btn(self):
+        # Note: Default when setting, user always select "Allow access" button
+        # Inbound
+        self.windows.verify_netfirewall_rule('Cốc Cốc (mDNS-In)', 'Inbound', 'Allow')
+        self.windows.verify_netfirewall_rule('Cốc Cốc (TCP-In)', 'Inbound', 'Allow')
+        self.windows.verify_netfirewall_rule('Cốc Cốc (UDP-In)', 'Inbound', 'Allow')
+        self.windows.verify_netfirewall_rule('Cốc Cốc Torrent Update (TCP-In)', 'Inbound', 'Allow')
+        self.windows.verify_netfirewall_rule('Cốc Cốc Torrent Update (UDP-In)', 'Inbound', 'Allow')
+        self.windows.verify_netfirewall_rule('Cốc Cốc (TCP-Out)', 'Outbound', 'Allow')
+        self.windows.verify_netfirewall_rule('Cốc Cốc (UDP-Out)', 'Outbound', 'Allow')
+        self.windows.verify_netfirewall_rule('Cốc Cốc Torrent Update (TCP-Out)', 'Outbound', 'Allow')
+        self.windows.verify_netfirewall_rule('Cốc Cốc Torrent Update (UDP-Out)', 'Outbound', 'Allow')
--- utils_automation/common.py
+++ utils_automation/common.py
@@ -5,6 +5,7 @@ import time
 import subprocess
 import psutil
 import win32api
+import re
 import glob
 from selenium import webdriver
 from os import path
@@ -26,6 +27,11 @@ def if_height_frame_so_width_frame(height_frame):
 
 def wait_for_stable(wait_time = 3):
     time.sleep(wait_time)
+
+def remove_spaces_in_string(string):
+    output = re.sub(' +', ' ', str(string))
+    print(output)
+    return output
 
 class CSVHandle:
     def get_from_csv(self, filename):
@@ -162,7 +168,7 @@ class WindowsCMD:
             output, error = process.communicate()
             print(output)
             return output
-        finally:
+        except:
             print("Cannot execute command!")
 
     def is_process_exists(process_name):
@@ -175,6 +181,19 @@ class WindowsCMD:
             except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
                 pass
         return False;
+
+class WindowsHandler:
+
+    def get_netfirewall_rule(self, display_name):
+        rule = WindowsCMD.execute_cmd("powershell get-netfirewallrule -DisplayName '" + display_name + "'")
+        string = remove_spaces_in_string(rule)
+        return string
+
+    def verify_netfirewall_rule(self, display_name, direction, action):
+        rule = self.get_netfirewall_rule(display_name)
+        assert "Direction : " + direction in rule
+        assert "Action : " + action in rule
+
 
 class BrowserHandler:
     def browser_init(self):
