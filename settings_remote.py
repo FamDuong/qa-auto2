2302162ae9ce6ed481b413c01ad8b808c090c8fd
bichngoc <bichngoc@coccoc.com>
2019-10-07 13:22:17 +0700

    Update CC version

--- settings_remote.py
+++ settings_remote.py
@@ -1,3 +1,3 @@
-COCCOC_VERSION = '76.0.3809.144'
+COCCOC_VERSION = '77.0.3865.104'
 USER_DATA_DIR = 'C:\\Users\\tester\\AppData\\Local\\CocCoc\\Browser\\User Data'
 HOST_FILE = r'C:\Windows\System32\drivers\etc'
