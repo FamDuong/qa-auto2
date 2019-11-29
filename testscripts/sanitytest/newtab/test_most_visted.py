005ed58bccd14b5bc01a86ff1d93f4799f7d256f
cuongld <cuongld@coccoc.com>
2019-11-29 16:14:04 +0700

    - Add step to decode current url cuz some character in url is encoded

--- testscripts/sanitytest/newtab/test_most_visted.py
+++ testscripts/sanitytest/newtab/test_most_visted.py
@@ -23,11 +23,13 @@ def test_check_redirect_after_click_on_most_visted(browser):
 
 @pytestrail.case('C54983')
 def test_check_redirect_after_click_on_most_visited_paid(browser):
+    import urllib.parse
     browser.get(Urls.NEW_TAB_URL)
     total_number_most_paid = new_tab_search_page_object.get_total_number_most_paid_sites(browser)
     random_element_number = random.randrange(total_number_most_paid - 1)
     new_tab_search_page_object.click_any_most_visited_paid_element(browser, random_element_number)
     current_url = browser.current_url
+    current_url = urllib.parse.unquote(current_url)
     assert 1 <= total_number_most_paid >= 5
     for utm_type in ['utm_source', 'utm_medium', 'utm_campaign', 'utm_content', 'utm_term']:
         assert re.search(rf'{utm_type}=[a-zA-Z0-9*]', current_url) is not None
