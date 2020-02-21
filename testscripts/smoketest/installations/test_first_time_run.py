3dd3654fb6127ea755d6aadbf36cfa83ec6d5ca6
cuongld <cuongld@coccoc.com>
2020-02-21 18:33:52 +0700

    Add test for check first time run C44829 QA-554

--- testscripts/smoketest/installations/test_first_time_run.py
+++ testscripts/smoketest/installations/test_first_time_run.py
@@ -1,7 +1,19 @@
+from pytest_testrail.plugin import pytestrail
 
 
 class TestFirstTimeRun:
 
+    def verify_new_tab_coccoc_exist(self):
+        from pywinauto import Desktop
+        coccoc_instance = Desktop(backend='uia').Welcome_to_Cốc_Cốc_Cốc_CốcPane
+        # existFlag = coccoc_instance.window(title="New Tab - Cốc Cốc").is_visible()
+        coccoc_instance.New_Tab.click_input()
+        all_windows = Desktop(backend='uia').windows()
+        # print(all_windows)
+        assert 'New Tab - Cốc Cốc' in str(all_windows)
+
+    @pytestrail.case('C44829')
+    @pytestrail.defect('BR-1398')
     def test_check_first_time_run(self):
         from testscripts.smoketest.common import uninstall_then_install_coccoc_with_default
         from testscripts.smoketest.common import change_default_browser
@@ -11,6 +23,11 @@ class TestFirstTimeRun:
         wait_for_window_appear(window_name='Import Google Chrome Settings')
         from testscripts.smoketest.common import choose_import_browser_settings
         choose_import_browser_settings(action='Cancel')
+        try:
+            self.verify_new_tab_coccoc_exist()
+        finally:
+            from testscripts.smoketest.common import cleanup
+            cleanup(firefox=False)
 
 
 
