8f2720f5c07a85376a57943a48023820659e302e
cuongld <cuongld@coccoc.com>
2020-04-17 15:16:45 +0700

    Fix for performance to add file

--- utils_automation/common.py
+++ utils_automation/common.py
@@ -66,6 +66,37 @@ class CSVHandle:
                 print("System error:", e)
                 return None
 
+    def write_result_data_for_page_load_time(self, file_name, keyname_list: list, value_list: list, result_type=''):
+        with open(file_name, 'a', encoding='utf-8') as file:
+            file.truncate(0)
+            total_value = 0
+            file.write(f"Results for {result_type} is as below :\n")
+            for (keyname, value) in zip(keyname_list, value_list):
+                file.write(f"{keyname} is: {value}")
+                file.write('\n')
+                total_value += value
+            average_value = total_value/len(value_list)
+            file.write(f"Average is : {average_value}")
+        file.close()
+
+    def write_result_data_for_cpu_ram(self, file_name, res, result_type=''):
+        with open(file_name, 'a', encoding='utf-8') as file:
+            file.truncate(0)
+            file.write(f"Results for {result_type} is as below :\n")
+            cpu_total = 0
+            mem_total = 0
+            for i in range(len(res)):
+                cpu_total += int(res[i].get("cpu"))
+                mem_total += int(res[i].get("mem"))
+                file.write("i is %d\n" % i)
+                file.write("CPU is %s, Mem is %s\n" % (res[i].get("cpu"), res[i].get("mem")))
+            cpu_average = cpu_total/len(res)
+            mem_average = mem_total/len(res)
+            file.write("Average value is :\n")
+            file.write(f"CPU : {cpu_average}\n")
+            file.write(f"MEM : {mem_average}")
+        file.close()
+
 
 class FilesHandle:
 
