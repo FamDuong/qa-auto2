f387e712892caef3dc2ee82a67b3900058174d9d
hangnt2 <hangnt2@coccoc.com>
2020-09-25 14:18:03 +0700

    QA-794: Verify ads on New tab page

--- testscripts/newtab_page/common.py
+++ testscripts/newtab_page/common.py
@@ -1,8 +1,10 @@
 import logging
 import time
 
+from models.pagelocators.newtab import NewTabMostVisitedLocators
 from models.pageobject.coccoc_search.coccoc_search_page_objects import CocCocSearchPageObjects
 from models.pageobject.newtab import NewTabAdsActions
+from testscripts.search.ccsearch_with_adblock_plus.common import verify_ads_is_oppened_in_newtab
 
 coccoc_search_page_object = CocCocSearchPageObjects()
 new_tab_ads_action = NewTabAdsActions()
@@ -32,13 +34,47 @@ def verify_most_visited_ads_in_newtab_page(driver, total_ads, ads_locator_xpath_
         for i in range(total_ads):
             coccoc_search_page_object.click_on_ad(driver, i + 1, ads_locator_xpath_by_index)
             ad_url = driver.current_url
-            LOGGER.info("Ad " + str(i) + ": " + ad_url)
+            if root_url not in ad_url:
+                LOGGER.info("Ad " + str(i + 1) + ": " + ad_url)
             from datetime import datetime
             start_time = datetime.now()
-            while root_url in ad_url:
-                time.sleep(1)
-                time_delta = datetime.now() - start_time
-                if time_delta.total_seconds() >= 15:
-                    break
+            if root_url in ad_url:
+                while root_url in ad_url:
+                    time.sleep(1)
+                    ad_url = driver.current_url
+                    LOGGER.info("Ad " + str(i + 1) + ": " + ad_url)
+                    time_delta = datetime.now() - start_time
+                    if time_delta.total_seconds() >= 15:
+                        break
             assert root_url not in ad_url
             driver.execute_script("window.history.go(-1)")
+
+
+def verify_most_visited_ads_steps(driver):
+    total_ads = new_tab_ads_action.count_all_most_visited_ads(driver)
+    verify_most_visited_ads_in_newtab_page(driver, total_ads,
+                                           NewTabMostVisitedLocators.MOST_VISITED_QC_BY_INDEX_XPATH)
+
+
+def verify_news_ads_steps(driver):
+    scroll_down_to_show_ads(driver, 0)
+    total_news_ads = new_tab_ads_action.count_all_news_ads(driver)
+    verify_ads_is_oppened_in_newtab(driver, total_news_ads, NewTabMostVisitedLocators.NEWS_ADS_BY_INDEX_XPATH)
+
+
+def check_most_visited_ads_with_url(driver, url_list):
+    if url_list is not None:
+        for url in url_list:
+            driver.get(url)
+            verify_most_visited_ads_steps(driver)
+    else:
+        verify_most_visited_ads_steps(driver)
+
+
+def check_news_ads_with_url(driver, url_list):
+    if url_list is not None:
+        for url in url_list:
+            driver.get(url)
+            verify_news_ads_steps(driver)
+    else:
+        verify_news_ads_steps(driver)
--- testscripts/newtab_page/conftest.py
+++ testscripts/newtab_page/conftest.py
@@ -2,16 +2,18 @@ import pytest
 
 
 def pytest_addoption(parser):
-    parser.addoption('--newtab-url', action='store', default='https://coccoc.com/webhp?forceAB=newsFeed:2,https://coccoc.com/webhp?forceAB=newsFeed:3')
+    parser.addoption('--newtab-url', action='store', )
+    # parser.addoption('--newtab-url', action='store', default='https://coccoc.com/webhp?forceAB=newsFeed:2,https://coccoc.com/webhp?forceAB=newsFeed:3')
 
 
 @pytest.fixture(scope='class')
 def get_newtab_url(request):
     newtab_url = request.config.getoption("--newtab-url")
-    if newtab_url is None:
-        raise Exception
-    newtab_url_values = newtab_url.split(",")
-    newtab_url_list = []
-    for each_url in newtab_url_values:
-        newtab_url_list.append(each_url)
-    return newtab_url_list
+    if newtab_url is not None:
+        newtab_url_values = newtab_url.split(",")
+        newtab_url_list = []
+        for each_url in newtab_url_values:
+            newtab_url_list.append(each_url)
+        return newtab_url_list
+    else:
+        return None
