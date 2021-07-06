3a39c59b7298e8897d3fdd4c94941d286936c5e0
nhung.nguyen <nhung.nguyen@coccoc.com>
2021-07-06 18:28:21 +0700

    QA-1495-update

---
+++ testscripts/search/safe_search/test_safe_search_off.py
@@ -0,0 +1,24 @@
+import csv
+
+import requests
+from utils_automation.common import FilesHandle, get_from_csv
+
+file_handle = FilesHandle()
+
+def test_safe_search():
+    newdata = []
+    header = ['query', 'is_empty_due_to_safe']
+    test_data = "../../../resources/test_data/safe_search_queries.csv"
+    query = get_from_csv(test_data)
+    for item in query:
+        response = requests.get(
+            "https://dev5.coccoc.com/composer?_=1625469249474&p=0&q="+item+"&reqid=cgYgveOl&apiV=1")
+        response.raise_for_status()
+        jsonResponse = response.json()
+        new_row = [item, str(jsonResponse['search']['is_empty_due_to_safe'])]
+        newdata.append(new_row)
+    with open(test_data, 'r+', encoding='utf-8', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(header)
+        writer.writerows(newdata)
+        file.close()
