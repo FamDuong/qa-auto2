394ce4aa77f04e03b59d27abd45a823624f2a2ca
bichngoc <bichngoc@coccoc.com>
2021-08-11 09:33:08 +0700

    Update kill browser

--- utils_automation/cleanup.py
+++ utils_automation/cleanup.py
@@ -4,5 +4,5 @@ import subprocess
 
 class Browsers:
     def kill_all_browsers(self):
-        prog = subprocess.Popen("taskkill /im chromedriver.exe /f", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        prog = subprocess.Popen("taskkill /im browser.exe /f", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         prog.communicate()
