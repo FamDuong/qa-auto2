56375af9dce406f5f92c825851eb6e1712bdec6b
bichngoc <bichngoc@coccoc.com>
2021-08-23 17:50:29 +0700

    Add method for checking by loading icon

--- testscripts/performancetest/test_browser_plt.py
+++ testscripts/performancetest/test_browser_plt.py
@@ -12,6 +12,7 @@ from utils_automation.cleanup import Browsers
 from utils_automation.url_utils import URLUtils
 from utils_automation.date_time_utils import get_current_timestamp
 from selenium.common.exceptions import TimeoutException
+from pywinauto import Desktop
 
 start_browser = 0
 LOGGER = logging.getLogger(__name__)
@@ -58,14 +59,15 @@ class TestPageLoadTime:
         driver = webdriver.Chrome(options=opts, desired_capabilities=caps)
         driver.set_page_load_timeout(30)
         try:
-            self.start_time = int(round(time.time() * 1000))
             driver.get(source)
-            WebDriverWait(driver, 10).until(lambda driver: driver.execute_script('return document.readyState') == 'complete')
-            self.end_time = int(round(time.time() * 1000))
         except TimeoutException as e:
             LOGGER.info("Page load Timeout occurred. Quiting !!!")
         return driver
 
+    def close_browser(self, driver):
+        driver.close()
+        driver.quit()
+        time.sleep(2)
 
     def measureTime(self, driver):
         global browser_startup
@@ -78,42 +80,50 @@ class TestPageLoadTime:
         backend_performance = response_start - navigation_start
         frontend_performance = dom_complete - response_start
         page_load_time = page_complete - navigation_start
-        page_load_time_by_state = self.end_time - self.start_time
 
         # LOGGER.info("Browser startup time: %s" % browser_startup)
         # LOGGER.info("First frame displayed: %s" % backend_performance)
         # LOGGER.info("DOM Load Event completed: %s" % frontend_performance)
         # LOGGER.info("Total PageLoad Time: %s" % page_load_time)
+        return page_load_time
 
-        time.sleep(2)
-        driver.quit()
-        return [page_load_time, page_load_time_by_state]
+    def measureTimebyLoadingIcon(self, get_browser_type):
+        if get_browser_type == "CocCoc":
+            coccoc_windows = Desktop(backend="uia").window(title_re='.* - Cốc Cốc.*')
+        else:
+            coccoc_windows = Desktop(backend="uia").window(title_re='.* - Chrome.*')
+        load_icon_omnibox = coccoc_windows.child_window(title="Reload", control_type="Button")
+        self.start_time = int(round(time.time() * 1000))
+        load_icon_omnibox.click_input()
+        load_icon_omnibox.wait("visible")
+        self.end_time = int(round(time.time() * 1000))
+        return self.end_time - self.start_time
 
     def get_page_load_time(self, filename, file_name_result, binary_file, default_dir, get_browser_type, options_list=None):
         listweb = get_from_csv(filename)
         loadtimes = []
-        loadtimes_by_state = []
+        loadtimes_by_loading_icon = []
         index = 1
-        LOGGER.info('%-30s' '%-30s' '%-30s' '%s' % ('No.', 'Url', 'PLT Average', 'PLT by State Average'))
+        LOGGER.info('%-30s' '%-30s' '%-30s' '%s' % ('No.', 'Url', 'PLT Average', 'PLT by Loading Icon Average'))
         for i in listweb:
             loadtime = 0
-            loadtime_by_state = 0
+            loadtime_by_loading_icon = 0
             looptime = 3
             for j in range(looptime):
                 browser = self.open_webpage(i, binary_file, default_dir, get_browser_type, options_list)
-                measure_time = self.measureTime(browser)
-                page_load_time = measure_time[0]
-                page_load_time_by_state = measure_time[1]
+                page_load_time = self.measureTime(browser)
+                page_load_time_by_loading_icon = self.measureTimebyLoadingIcon(get_browser_type)
+                self.close_browser(browser)
                 loadtime = loadtime + page_load_time
-                loadtime_by_state = loadtime_by_state + page_load_time_by_state
-                LOGGER.info('%-30s' '%-30s' '%-30s' '%s' % (str(index) + "." + str(j), i, page_load_time, page_load_time_by_state))
+                loadtime_by_loading_icon = loadtime_by_loading_icon + page_load_time_by_loading_icon
+                LOGGER.info('%-30s' '%-30s' '%-30s' '%s' % (str(index) + "." + str(j), i, page_load_time, page_load_time_by_loading_icon))
             page_load_time_avg = round(loadtime / looptime, 1)
-            page_load_time_by_state_avg = round(loadtime_by_state / looptime, 1)
+            page_load_time_by_loading_icon_avg = round(loadtime_by_loading_icon / looptime, 1)
             loadtimes.append(page_load_time_avg)
-            loadtimes_by_state.append(page_load_time_avg)
-            LOGGER.info('%-30s' '%-30s' '%-30s' '%s' % ("Average", i, page_load_time_avg, page_load_time_by_state_avg))
+            loadtimes_by_loading_icon.append(page_load_time_by_loading_icon_avg)
+            LOGGER.info('%-30s' '%-30s' '%-30s' '%s' % ("Average", i, page_load_time_avg, page_load_time_by_loading_icon_avg))
             index += 1
-            write_result_data_for_page_load_time(file_name=file_name_result, keyname_list=i, value_list=[page_load_time_avg, page_load_time_by_state_avg],
+            write_result_data_for_page_load_time(file_name=file_name_result, keyname_list=i, value_list=[page_load_time_avg, page_load_time_by_loading_icon_avg],
                                                  result_type='Page load time')
         page_load_time_avg = round(sum(loadtimes) / len(listweb), 1)
         write_result_data_for_page_load_time(file_name=file_name_result, keyname_list="Average",
@@ -121,7 +131,7 @@ class TestPageLoadTime:
                                              result_type='Page load time')
 
     @pytestrail.case('C82299')
-    def test_browser_plt(self, binary_path, default_directory, application_path, get_browser_type):
+    def test_browser_plt(self, binary_path, default_directory, application_path, get_browser_type = "CocCoc"):
         LOGGER.info("Run in %s" % get_browser_type)
         dirname, runname = os.path.split(os.path.abspath(__file__))
         filename = dirname + r'\test_data' + r"\testbenchmark.csv"
